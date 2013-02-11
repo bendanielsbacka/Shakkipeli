0dd97523e533bf3eb6159857b9e1ffb3fe9777c5
Ben Danielsbacka <ben.danielsbacka@outlook.com>
2013-02-11 17:41:58 +0200

    Testi

---
+++ src/Nappula.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 11.2.2013
+ * Time: 12:54
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Nappula {
+    int testi();
+}
